89108a26d0db458f836f9ebbbb56f40542d934d5
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2020-10-01 20:59:26 +0200

    Migrations

--- FvpWebApp/Data/Migrations/20201001185859_Documents.Designer.cs
+++ FvpWebApp/Data/Migrations/20201001185859_Documents.Designer.cs
@@ -4,14 +4,16 @@ using FvpWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FvpWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201001185859_Documents")]
+    partial class Documents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
