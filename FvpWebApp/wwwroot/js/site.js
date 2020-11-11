c2bfa4644badd2541261f880120644213f60d09f
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2020-11-11 16:57:30 +0100

    Vue elements

--- FvpWebApp/wwwroot/js/site.js
+++ FvpWebApp/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿var datatableLanguage = {
+﻿
+var datatableLanguage = {
     "processing": "Przetwarzanie...",
     "search": "Szukaj:",
     "lengthMenu": "Pokaż _MENU_ pozycji",
@@ -20,3 +21,71 @@
         "sortDescending": ": aktywuj, by posortować kolumnę malejąco"
     }
 };
+
+var documentData = {
+    document: {
+        documentId,
+        externalId,
+        sourceId,
+        contractorId,
+        taskTicketId,
+        documentNumber,
+        documentSymbol,
+        saleDate,
+        documentDate,
+        net,
+        gross,
+        vat,
+        jpkV7,
+        documentStatus,
+        docContractorId,
+        docContractorName,
+        docContractorVatId,
+        docContractorCity,
+        docContractorPostCode,
+        docContractorCountryCode,
+        docContractorStreetAndNumber,
+        docContractorFirm,
+        createdAt,
+        updatedAt,
+        updatedBy,
+        documentVats: [
+            {
+                documentVatId,
+                vatCode,
+                vatValue,
+                vatAmount,
+                netAmount,
+                grossAmount,
+                vatTags,
+                documentId,
+            },
+        ],
+    },
+    contractors: [
+        {
+            contractorId,
+            contractorErpId,
+            contractorErpPosition,
+            sourceId,
+            gusContractorEntriesCount,
+            contractorSourceId,
+            name,
+            street,
+            estateNumber,
+            quartersNumber,
+            city,
+            postalCode,
+            province,
+            vatId,
+            regon,
+            phone,
+            email,
+            countryCode,
+            firm,
+            contractorStatus,
+            checkDate,
+            documents,
+        },
+    ],
+}
